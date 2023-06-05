004e7046975f7369809c14370a5fd11813866f27
nmsantaana1994 <nmsantaana1994@gmail.com>
2023-06-05 13:27:29 -0300

    Empezando con la carga de los partidos

---
+++ src/services/partidos.js
@@ -0,0 +1,8 @@
+export async function cargarPartido ({fields /*Campos que se pidieron en el form*/}) {
+    try {
+        
+    } catch (err) {
+        console.log("[partidos.js cargarPartido()] Error al cargar el partido", err);
+        throw err;
+    }
+}
---
+++ src/views/CargaPartido.vue
@@ -0,0 +1,25 @@
+<script setup>
+import {ref} from "vue";
+import { useRouter } from "vue-router";
+import { cargarPartido } from "../services/partidos.js";
+
+const {fields, loading, handleSubmit} = useCargaPartido();
+
+function useCargaPartido() {
+    const fields = ref({
+        // Campos a pedir en la carga del form.
+    });
+
+    const loading = ref(false);
+
+    async function handleSubmit() {
+        loading.value = true;
+
+        await cargarPartido()
+    }
+}
+</script>
+
+<template>
+
+</template>
